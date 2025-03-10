8bc8ffaad0aebadba90f33cef926df80cc46efe8
jackson mativo <jacksonmativo21@gmail.com>
2025-03-10 09:25:33 -0400

    Complete variables lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+// 1. Declare companyName as a constant with the value "Scuber"
+const companyName = "Scuber";
+
+// 2. Declare mostProfitableNeighborhood as a variable using let
+let mostProfitableNeighborhood = "Chelsea";
+
+// 3. Declare companyCeo as a variable using let
+let companyCeo = "Susan Smith";
